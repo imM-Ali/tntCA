05a0dfa2ec051d248f054f75b65d8d38e3dbca95
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2020-11-16 13:07:39 +0000

    Added project

---
+++ tntCA/src/tntca/TntCA.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tntca;
+
+/**
+ *
+ * @author Ali
+ */
+public class TntCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // I WILL ADD THE MAIN METHOD (ALI)
+        
+        
+    }
+    public static void Add(int a,int b){
+        // YURI PLEASE ADD YOUR CODE HERE FOR ADDITION OF a AND b
+    }
+    public static void Multiply(int a,int b){
+        // RENATO PLEASE ADD YOUR CODE HERE FOR MULTIPLICATION OF a AND b
+    }
+    public static void Subtract(int a,int b){
+        // HAIDER PLEASE ADD YOUR CODE HERE FOR SUBTRACTION OF a AND b
+    }
+    public static void Greet(int a,int b){
+        // THIAGO PLEASE ADD YOUR CODE HERE FOR SAYING HELLO WORLD!
+    }
+    
+    
+}
