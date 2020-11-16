fe2b27c6aebd822ac0fd46c51fa04c696ac7d63a
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2020-11-16 13:49:34 +0000

    Added my code.
    Please enter the code which is assigned to you. Thanks!

--- tntCA/src/tntca/TntCA.java
+++ tntCA/src/tntca/TntCA.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package tntca;
-
+import java.util.*;
 /**
  *
  * @author Ali
@@ -16,11 +16,37 @@ public class TntCA {
      */
     public static void main(String[] args) {
         // I WILL ADD THE MAIN METHOD (ALI)
+        int input1,input2;
+        String select;
+        Scanner myKb= new Scanner(System.in);
         
+        System.out.println("Please Enter the first number");
+        input1 = myKb.nextInt();
+        System.out.println("Please Enter the next number");
+        input2 = myKb.nextInt();
+        System.out.println("Press A to add, Press S to subtract, Press M to multiply, Press G for a greeting!");
+        select = myKb.next();
+        
+        switch(select.charAt(0)){
+            case 'A':
+            case 'a': Add(input1,input2); break;
+            case 'S':
+            case 's': Subtract(input1,input2); break;
+            case 'M':
+            case 'm': Multiply(input1,input2); break;
+            case 'G':
+            case 'g': Greet(); break;
+            default:
+                break;
+                
+        }
         
     }
+    //REMEMBER GUYS YOU HAVE TO VARIABLES TO WORK WITH, a AND b, you just have to write the code and 1 line for output, thats it!
     public static void Add(int a,int b){
         // YURI PLEASE ADD YOUR CODE HERE FOR ADDITION OF a AND b
+        
+        
     }
     public static void Multiply(int a,int b){
         // RENATO PLEASE ADD YOUR CODE HERE FOR MULTIPLICATION OF a AND b
@@ -28,7 +54,7 @@ public class TntCA {
     public static void Subtract(int a,int b){
         // HAIDER PLEASE ADD YOUR CODE HERE FOR SUBTRACTION OF a AND b
     }
-    public static void Greet(int a,int b){
+    public static void Greet(){
         // THIAGO PLEASE ADD YOUR CODE HERE FOR SAYING HELLO WORLD!
     }
     
